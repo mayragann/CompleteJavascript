d688befa8e83a330ce55dd84ab57398c8b073319
mayragann <mayragann@gmail.com>
2022-09-28 15:54:16 -0400

    More Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,71 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas')
+// console.log('Jonas')
 
-let firstName = 'Matilda';
+// let firstName = 'Matilda';
 
-console.log(firstName);
+// console.log(firstName);
 
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
 
-let country = 'United States';
-let continent = 'North America';
+//test
+// let country = 'United States';
+// let continent = 'North America';
 let population = 3;
-console.log(country, continent, population)
+// let isIsland = false;
+// let language;
+// console.log(typeof country, typeof continent, typeof population, typeof isIsland)
+
+
+
+// dynamic typing example
+// let javascriptIsFun = true;
+// console.log(typeof true);
+// console.log(typeof 1);
+// console.log(typeof "String");
+// console.log(javascriptIsFun)
+
+
+// javascriptIsFun = 'YES!'
+// console.log(javascriptIsFun)
+// // dynamic typing example
+// //undefined example
+// let year;
+// console.log(year)
+// console.log(typeof year)
+
+// year = 1991;
+// console.log(typeof year)
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+//math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2**3)
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas'
+const lastName = 'Oddname'
+console.log(firstName + ' ' +lastName)
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x ++ // x = x + 1
+x --; // x = x -1
+x --; // x = x -1
+console.log(x)
+
+console.log(ageJonas < ageSarah);
+console.log(ageSarah >= 18);
