535de70beb07e20e801fe886a444455760e3704a
mayragann <mayragann@gmail.com>
2022-09-28 14:35:16 -0400

    Values and Variables added

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas')
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let country = 'United States';
+let continent = 'North America';
+let population = 3;
+console.log(country, continent, population)
