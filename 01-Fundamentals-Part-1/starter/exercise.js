debdc142413b981ad665b55f4788a1cfbf240509
mayragann <mayragann@gmail.com>
2022-10-04 14:32:10 -0400

    Added Additional Exercises

--- 01-Fundamentals-Part-1/starter/exercise.js
+++ 01-Fundamentals-Part-1/starter/exercise.js
@@ -16,13 +16,34 @@ language = "English";
 const country="United States"
 const continent = 'North America';
 const isIsland = false;
-isIsland = true
-
+// isIsland = true
+const finland = 6;
+const avgPopulation = 33;
+const description1 = country + ' is in ' + continent + ' it has ' + population + ' and its people speak ' + language
 
 console.log(typeof country)
 console.log(typeof continent)
 console.log(typeof population)
 console.log(typeof language)
+// 1. If your country split in half, and each half would contain half the population,
+console.log(population / 2);
+// then how many people would live in each half?
+// 2. Increase the population of your country by 1 and log the result to the console
+population++;
+console.log(population)
+// 3. Finland has a population of 6 million. Does your country have more people than
+// Finland?
+console.log(population > finland)
+console.log(population < finland)
+// 4. The average population of a country is 33 million people. Does your country
+// have less people than the average country?
+console.log(population < avgPopulation)
+
+// 5. Based on the variables you created, create a new variable 'description'
+// which contains a string with this format: 'Portugal is in Europe, and its 11 million
+// people speak portuguese'
+
+console.log(description1)
 
 
 
