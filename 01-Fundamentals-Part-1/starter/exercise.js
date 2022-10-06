5e8bfc2217103731ec546214a9172ce36e6f2973
mayragann <mayragann@gmail.com>
2022-10-06 15:03:45 -0400

    Completed Second Challenge

--- 01-Fundamentals-Part-1/starter/exercise.js
+++ 01-Fundamentals-Part-1/starter/exercise.js
@@ -12,62 +12,72 @@ let population = 326;
 // console.log(isIsland)
 // console.log(language)
 
-language = "English";
-const country="United States"
-const continent = 'North America';
-const isIsland = false;
-// isIsland = true
-const finland = 6;
-const avgPopulation = 33;
-const description1 = country + ' is in ' + continent + ' it has ' + population + ' and its people speak ' + language
+// language = "English";
+// const country="United States"
+// const continent = 'North America';
+// const isIsland = false;
+// // isIsland = true
+// const finland = 6;
+// const avgPopulation = 33;
+// const description1 = country + ' is in ' + continent + ' it has ' + population + ' and its people speak ' + language
 
-console.log(typeof country)
-console.log(typeof continent)
-console.log(typeof population)
-console.log(typeof language)
-// 1. If your country split in half, and each half would contain half the population,
-console.log(population / 2);
-// then how many people would live in each half?
-// 2. Increase the population of your country by 1 and log the result to the console
-population++;
-console.log(population)
-// 3. Finland has a population of 6 million. Does your country have more people than
-// Finland?
-console.log(population > finland)
-console.log(population < finland)
-// 4. The average population of a country is 33 million people. Does your country
-// have less people than the average country?
-console.log(population < avgPopulation)
+// console.log(typeof country)
+// console.log(typeof continent)
+// console.log(typeof population)
+// console.log(typeof language)
+// // 1. If your country split in half, and each half would contain half the population,
+// console.log(population / 2);
+// // then how many people would live in each half?
+// // 2. Increase the population of your country by 1 and log the result to the console
+// population++;
+// console.log(population)
+// // 3. Finland has a population of 6 million. Does your country have more people than
+// // Finland?
+// console.log(population > finland)
+// console.log(population < finland)
+// // 4. The average population of a country is 33 million people. Does your country
+// // have less people than the average country?
+// console.log(population < avgPopulation)
 
 // 5. Based on the variables you created, create a new variable 'description'
 // which contains a string with this format: 'Portugal is in Europe, and its 11 million
 // people speak portuguese'
 
-console.log(description1)
-const description = `My country is ${country}, and is in ${continent} it has ${population} million and its people speak ${language}`
-console.log(description)
+// console.log(description1)
+// const description = `My country is ${country}, and is in ${continent} it has ${population} million and its people speak ${language}`
+// console.log(description)
 
-if(population > 33){
-    console.log(
-    `${country} is above average`
-    );
-}
-else{
-    console.log(
-        `${country}'s is ${33 - population} below average`)
-}
+// if(population > 33){
+//     console.log(
+//     `${country} is above average`
+//     );
+// }
+// else{
+//     console.log(
+//         `${country}'s is ${33 - population} below average`)
+// }
 // let markWeight = 78;
 // let markHeight = 1.69;
 // let johnWeight = 92;
 // let johnHeight = 1.95;
 
-// const markBMI = (markWeight / markHeight ** 2);
+// const markBMI = markWeight / markHeight ** 2;
 // console.log(markBMI);
 // const johnBMI = (johnWeight / johnHeight ** 2);
 // console.log(johnBMI);
 
-// const markHigherBMI = markBMI > johnBMI;
-// console.log(markHigherBMI);
+
+// if(markBMI > johnBMI){
+//     console.log(
+//         `Mark's BMI: ${markBMI} is higher than John's BMI: ${johnBMI}`
+//     );
+// }
+//     else{
+//         console.log(
+//             `John's BMI: ${johnBMI} is higher than Mark's BMI: ${markBMI}`
+//         );
+//     }
+
 
 
 
