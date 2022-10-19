d4ecf05184776632cb1da756c5c76550b6b93eed
mayragann <mayragann@gmail.com>
2022-10-19 15:20:28 -0400

    Functions Declartations and Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,33 +1,52 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can Drive");
+// if(passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can Drive");
 
 //!reserved features for 'use strict' for future releases
 // const interface = 'Audio';
 // const if = 23; -> can't call it that because it's already being use for if/else statement
 
-//!Functions
+//!Functions declarations
 //? piece of code that can be reused
 //? can hold 1 or more complete lines of code
+//? can be called before defined
 
-function logger(){
-    console.log('My name is Mayra');
-}
+// function logger(){
+//     console.log('My name is Mayra');
+// }
 
-logger();//invoking function calling function, or running function
-logger();
+// logger();//invoking function calling function, or running function
+// logger();
 
-function fruitProcessor(apples,oranges){
+// function fruitProcessor(apples,oranges){
   
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
-}
-//need to store value when calling, give arguments
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice)
-const appleOrangeJuice = fruitProcessor(5, 10);
-console.log(appleOrangeJuice)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// //need to store value when calling, give arguments
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice)
+// const appleOrangeJuice = fruitProcessor(5, 10);
+// console.log(appleOrangeJuice)
+
+// function calcAgeOne(birthYear){
+//     return 2037 - birthYear;;
+// }
+
+// const ageOne = calcAgeOne(1991);
+// console.log(ageOne)
+
+// //!Function Expression
+// //? does not work if declared first 
+// //*makes code a little nicer and more structured. 
+// //*Can store everything in variables
+
+// const calcAgeTwo = function (birthYear){
+//     return 2037 - birthYear;;
+// }
+// const ageTwo = calcAgeTwo(1993);
+// console.log(ageOne, ageTwo)
