6ca1786bba08acd26e49d6ee516e4a897aa062ac
mayragann <mayragann@gmail.com>
2022-10-19 15:20:47 -0400

    Added Function Declaration and Expression Exercises

--- 02-Fundamentals-Part-2/starter/exercise.js
+++ 02-Fundamentals-Part-2/starter/exercise.js
@@ -1,12 +1,27 @@
 'use strict';
 
-function describeCountry(country, population, capitalCity){
-    const countryInfo = `${country} has ${population} and a capital of ${capitalCity}`
-    return countryInfo;
-}
+// function describeCountry(country, population, capitalCity){
+//     const countryInfo = `${country} has ${population} and a capital of ${capitalCity}`
+//     return countryInfo;
+// }
 
-const countryOne = describeCountry('United States', '336 Million', 'Washington DC')
-console.log(countryOne)
-const countryTwo = describeCountry('Mexico', '356 Million', 'Mexico City')
-console.log(countryTwo)
+// const countryOne = describeCountry('United States', '336 Million', 'Washington DC')
+// console.log(countryOne)
+// const countryTwo = describeCountry('Mexico', '356 Million', 'Mexico City')
+// console.log(countryTwo)
 
+// //? function declartions vs expressions
+// //declaration first
+// function percetageOfWorld(population){
+//     const worldPopulation = 7900
+//     return (population/worldPopulation)*100
+// }
+// const percetageOfWorldOne = percetageOfWorld(1441)
+// console.log(percetageOfWorldOne)
+// //expression
+// const percentOfWorldTwo = function(population){
+//     const worldPopulation = 7900
+//     return (population/worldPopulation)*100
+// }
+// const chinaPercent = percentOfWorldTwo(1441)
+// console.log(chinaPercent)
