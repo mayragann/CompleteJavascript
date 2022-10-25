3772b1d9cdbd9eaebe914dca48076a72e7fc640f
mayragann <mayragann@gmail.com>
2022-10-25 14:43:01 -0400

    Functions calling other Functions

--- 02-Fundamentals-Part-2/starter/exercise.js
+++ 02-Fundamentals-Part-2/starter/exercise.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // function describeCountry(country, population, capitalCity){
 //     const countryInfo = `${country} has ${population} and a capital of ${capitalCity}`
@@ -12,12 +12,12 @@
 
 // //? function declartions vs expressions
 // //declaration first
-// function percetageOfWorld(population){
-//     const worldPopulation = 7900
-//     return (population/worldPopulation)*100
-// }
-// const percetageOfWorldOne = percetageOfWorld(1441)
-// console.log(percetageOfWorldOne)
+function percetageOfWorld(population) {
+  const worldPopulation = 7900;
+  return (population / worldPopulation) * 100;
+}
+const percetageOfWorldOne = percetageOfWorld(1441);
+console.log(percetageOfWorldOne);
 // //expression
 // const percentOfWorldTwo = function(population){
 //     const worldPopulation = 7900
@@ -28,3 +28,18 @@
 
 // const percentageOfworld = population => (population/7900)*100
 // console.log(percentageOfworld(1441))
+
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+
+function describePopulation(country, population) {
+  const populationTotal = percetageOfWorld(population);
+  const totalPopulation = `${country} has a total population of ${population} millions which is ${populationTotal}`;
+  return totalPopulation;
+}
+console.log(describePopulation("China", 1441));
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -65,3 +65,17 @@
 //     return `${firstName} retires in ${retirement} years`;
 // }
 // console.log(yearsUntilRetirement(1989, 'Mayra'))
+
+//! Calling Other Functions
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+function fruitProcessor(apples,oranges){
+    const applePieces = cutFruitPieces(apples)
+    const orangePieces = cutFruitPieces(oranges)
+  
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+    return juice;
+}
+console.log(fruitProcessor(2,3))
